a046a79fd15a42d596554beaec68ed10e1b4e4eb
Tom Gregor <tgregor@ivsoftware.com>
2023-02-09 05:51:02 -0700

    SFSG

---
+++ move-picture-box-with-arrow-keys/MainForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace move_picture_box_with_arrow_keys
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxGreen = new move_picture_box_with_arrow_keys.ArrowKeyPictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBoxBlue = new move_picture_box_with_arrow_keys.ArrowKeyPictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxGreen
+            // 
+            this.pictureBoxGreen.BackColor = System.Drawing.Color.LightGreen;
+            this.pictureBoxGreen.IsMoveTarget = false;
+            this.pictureBoxGreen.Location = new System.Drawing.Point(24, 45);
+            this.pictureBoxGreen.Name = "pictureBoxGreen";
+            this.pictureBoxGreen.Size = new System.Drawing.Size(60, 60);
+            this.pictureBoxGreen.TabIndex = 0;
+            this.pictureBoxGreen.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(24, 213);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(425, 184);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBoxBlue
+            // 
+            this.pictureBoxBlue.BackColor = System.Drawing.Color.LightBlue;
+            this.pictureBoxBlue.IsMoveTarget = false;
+            this.pictureBoxBlue.Location = new System.Drawing.Point(101, 45);
+            this.pictureBoxBlue.Name = "pictureBoxBlue";
+            this.pictureBoxBlue.Size = new System.Drawing.Size(60, 60);
+            this.pictureBoxBlue.TabIndex = 0;
+            this.pictureBoxBlue.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 409);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBoxBlue);
+            this.Controls.Add(this.pictureBoxGreen);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlue)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ArrowKeyPictureBox pictureBoxGreen;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private ArrowKeyPictureBox pictureBoxBlue;
+    }
+}
+
---
+++ move-picture-box-with-arrow-keys/MainForm.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace move_picture_box_with_arrow_keys
+{
+    public partial class MainForm : Form , IMessageFilter
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Application.AddMessageFilter(this);
+            Disposed += (sender, e) =>Application.RemoveMessageFilter(this);
+        }
+        const int WM_KEYDOWN = 0x0100;
+        public bool PreFilterMessage(ref Message m)
+        {
+            base.WndProc(ref m);
+            switch (m.Msg)
+            {
+                case WM_KEYDOWN:
+                    foreach (var target in Controls.OfType<ArrowKeyPictureBox>())
+                    {
+                        target.MoveProgrammatically((Keys)m.WParam);
+                    }
+                    break;
+            }
+            return false;
+        }
+    }
+    class ArrowKeyPictureBox : PictureBox
+    {
+        const int INCREMENT = 1;
+        public void MoveProgrammatically(Keys direction)
+        {
+            Rectangle preview = Bounds;
+            if (IsMoveTarget)
+            {
+                BringToFront();
+                switch (direction)
+                {
+                    case Keys.Up:
+                        if (Top <= 0)
+                        {
+                            return;
+                        }
+                        preview.Y -= INCREMENT;
+                        if(detectCollision())
+                        {
+                            return;
+                        }
+                        Top -= INCREMENT;
+                        break;
+                    case Keys.Down:
+                        if (Bottom >= Parent.Bottom)
+                        {
+                            return;
+                        }
+                        if (detectCollision())
+                        {
+                            return;
+                        }
+                        Top += INCREMENT;
+                        break;
+                    case Keys.Left:
+                        if (Left <= 0)
+                        {
+                            return;
+                        }
+                        if (detectCollision())
+                        {
+                            return;
+                        }
+                        Left -= INCREMENT;
+                        break;
+                    case Keys.Right:
+                        if (Right >= Parent.Right)
+                        {
+                            return;
+                        }
+                        if (detectCollision())
+                        {
+                            return;
+                        }
+                        Left += INCREMENT;
+                        break;
+                }
+            }
+            bool detectCollision()
+            {
+                foreach (Control control in Parent.Controls)
+                {
+                    if(!ReferenceEquals(this, control))
+                    {
+                        if(preview.IntersectsWith(control.Bounds))
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+            foreach (var control in Parent.Controls.OfType<ArrowKeyPictureBox>()) 
+            { 
+                if(!ReferenceEquals(control, this)) control.IsMoveTarget= false;
+            }
+            IsMoveTarget= true;
+        }
+        bool _isMoveTarget = false;
+
+        public bool IsMoveTarget
+        {
+            get => _isMoveTarget;
+            set
+            {
+                if (!Equals(_isMoveTarget, value))
+                {
+                    _isMoveTarget = value;
+                    BorderStyle = IsMoveTarget ?
+                        BorderStyle.FixedSingle :
+                        BorderStyle.None;
+                }
+            }
+        }
+    }
+}
